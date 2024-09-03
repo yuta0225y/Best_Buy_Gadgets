1cdbe78dc1fd6320260c8f478897aa7cf7961ad8
yuta <yas0225y@gmail.com>
2024-09-03 10:22:23 +0900

    i18nやその他の修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  get 'search_gadgets', to: 'boards#search_gadgets'
 end
