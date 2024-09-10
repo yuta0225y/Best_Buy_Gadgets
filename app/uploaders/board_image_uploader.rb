56518928eb5b3868cf91b3e9472a11b0759d868f
yuta <yas0225y@gmail.com>
2024-09-10 12:56:38 +0900

    Dockerfileにimagemajickを追加

--- app/uploaders/board_image_uploader.rb
+++ app/uploaders/board_image_uploader.rb
@@ -1,28 +1,23 @@
 class BoardImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick # 画像サイズ調整のためにMiniMagickをinclude
 
-  if Rails.env.production?
+  if Rails.env.production? # 本番環境の場合はS3に保存
     storage :fog
   else
     storage :file
   end
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}" # 画像の保存先の指定
   end
 
   def default_url
-    ActionController::Base.helpers.asset_path('board_placeholder.png')
+    ActionController::Base.helpers.asset_path('board_placeholder.png')  # 画像がない場合のデフォルト画像
   end
 
-  # 画像のリサイズ設定を確認
-  version :thumb do
-    process resize_to_fill: [400, 400] # 幅400px、高さ300pxにリサイズ
-  end
+    process resize_to_fit: [400, 400] # 画像サイズの上限を400x400にする
 
-  def extension_allowlist
+  def extension_allowlist # 画像の拡張子を許可
     %w[jpg jpeg gif png]
   end
-
-  # filenameの設定は省略しておきます。
 end
